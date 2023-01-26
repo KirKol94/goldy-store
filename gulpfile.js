ce8598b2a4a20bfdbf35b8570e194df153e10f3a
Kirill Kolchanov <kirkol94@ya.ru>
2023-01-27 00:00:50 +0500

    подправил gulpfile, теперь с картинками все хорошо :)

--- gulpfile.js
+++ gulpfile.js
@@ -1,61 +1,63 @@
-const { src, dest, watch, series, parallel } = require("gulp");
-const del = require("del");
-const pug = require("gulp-pug");
-const sass = require("gulp-sass")(require("sass"));
-const gulpGroupCssMediaQueries = require("gulp-group-css-media-queries");
-const cssNano = require("gulp-cssnano");
-const autoprefixer = require("gulp-autoprefixer");
-const rename = require("gulp-rename");
-const webp = require("gulp-webp");
-const svgSprite = require("gulp-svg-sprite");
-const htmlWebp = require("gulp-webp-html-nosvg");
-const browserSync = require("browser-sync").create();
-const gulpIf = require("gulp-if");
-const rigger = require("gulp-rigger");
-const uglify = require("gulp-uglify");
+const {
+ src, dest, watch, series, parallel,
+} = require('gulp')
+const del = require('del')
+const pug = require('gulp-pug')
+const sass = require('gulp-sass')(require('sass'))
+const gulpGroupCssMediaQueries = require('gulp-group-css-media-queries')
+const cssNano = require('gulp-cssnano')
+const autoprefixer = require('gulp-autoprefixer')
+const rename = require('gulp-rename')
+const webp = require('gulp-webp')
+const svgSprite = require('gulp-svg-sprite')
+const htmlWebp = require('gulp-webp-html-nosvg')
+const browserSync = require('browser-sync').create()
+const gulpIf = require('gulp-if')
+const rigger = require('gulp-rigger')
+const uglify = require('gulp-uglify')
 
-const isBuild = process.argv.includes("--build");
-const isDev = !isBuild;
+const isBuild = process.argv.includes('--build')
+const isDev = !isBuild
 
 const path = {
 	html: {
-		src: "src/pug",
-		dist: "build",
+		src: 'src/pug',
+		dist: 'build',
 	},
 	css: {
-		src: "src/scss",
-		dist: "build",
+		src: 'src/scss',
+		dist: 'build',
 	},
 	js: {
-		src: "src/js",
-		dist: "build/js",
+		src: 'src/js',
+		dist: 'build/js',
 	},
 	img: {
-		src: "src/img",
-		dist: "build/img",
+		src: 'src/img',
+		dist: 'build/img',
 	},
 	svg: {
-		src: "src/img/svg",
-		dist: "build/svg",
+		src: 'src/img/svg',
+		dist: 'build/svg',
 	},
 	fonts: {
-		src: "src/fonts",
-		dist: "build/fonts",
+		src: 'src/fonts',
+		dist: 'build/fonts',
 	},
 	favicon: {
-		src: "src/favicon",
-		dist: "build/favicon",
+		src: 'src/favicon',
+		dist: 'build/favicon',
 	},
-	build: "build",
-	src: "src",
-};
+	build: 'build',
+	src: 'src',
+}
 
 function htmlGenerator() {
 	return src(`${path.html.src}/*.pug`)
 		.pipe(pug(gulpIf(isDev, { pretty: true })))
-		.pipe(gulpIf(isBuild, htmlWebp()))
+		// .pipe(gulpIf(isBuild, htmlWebp()))
 		.pipe(dest(path.html.dist))
-		.pipe(browserSync.stream());
+		.pipe(browserSync.stream())
 }
 
 function styleGenerator() {
@@ -68,12 +70,12 @@ function styleGenerator() {
 				isBuild,
 				autoprefixer({
 					cascade: false,
-				})
-			)
+				}),
+			),
 		)
-		.pipe(rename("style.css"))
+		.pipe(rename('style.css'))
 		.pipe(dest(path.css.dist))
-		.pipe(browserSync.stream());
+		.pipe(browserSync.stream())
 }
 
 function bundleJsGenerator() {
@@ -81,14 +83,14 @@ function bundleJsGenerator() {
 		.pipe(rigger())
 		.pipe(uglify())
 		.pipe(dest(path.js.dist))
-		.pipe(browserSync.stream());
+		.pipe(browserSync.stream())
 }
 
 function imagesConverter() {
 	return src(`${path.img.src}/**/*.{jpg,jpeg,png,gif}`)
-		.pipe(gulpIf(isBuild, webp()))
+		// .pipe(gulpIf(isBuild, webp()))
 		.pipe(dest(path.img.dist))
-		.pipe(browserSync.stream());
+		.pipe(browserSync.stream())
 }
 
 function svgConverter() {
@@ -97,25 +99,25 @@ function svgConverter() {
 			svgSprite({
 				mode: {
 					stack: {
-						sprite: "../sprite.svg",
+						sprite: '../sprite.svg',
 					},
 				},
-			})
+			}),
 		)
 		.pipe(dest(path.svg.dist))
-		.pipe(browserSync.stream());
+		.pipe(browserSync.stream())
 }
 
 function favicon() {
 	return src(`${path.favicon.src}/**/*.*`)
 		.pipe(dest(path.favicon.dist))
-		.pipe(browserSync.stream());
+		.pipe(browserSync.stream())
 }
 
 function fonts() {
 	return src(`${path.fonts.src}/**/*.{ttf,otf,woff,woff2}`)
 		.pipe(dest(path.fonts.dist))
-		.pipe(browserSync.stream());
+		.pipe(browserSync.stream())
 }
 
 function server() {
@@ -123,36 +125,36 @@ function server() {
 		server: {
 			baseDir: path.build,
 		},
-	});
+	})
 
-	watch(`${path.src}/**/*.*`).on("change", browserSync.reload);
+	watch(`${path.src}/**/*.*`).on('change', browserSync.reload)
 }
 
 function clear() {
-	return del(path.build);
+	return del(path.build)
 }
 
 function clearImg() {
-	return del(`${path.build}/img`);
+	return del(`${path.build}/img`)
 }
 
 function clearFonts() {
-	return del(`${path.build}/fonts`);
+	return del(`${path.build}/fonts`)
 }
 
 function watcher() {
-	watch(`${path.html.src}/**/*.pug`, htmlGenerator);
-	watch(`${path.css.src}/**/*.scss`, styleGenerator);
+	watch(`${path.html.src}/**/*.pug`, htmlGenerator)
+	watch(`${path.css.src}/**/*.scss`, styleGenerator)
 	watch(
 		`${path.img.src}/**/*.{jpg,jpeg,png,gif,}`,
-		series(clearImg, imagesConverter)
-	);
-	watch(`${path.svg.src}/**/*.svg`, svgConverter);
-	watch(`${path.fonts.src}/**/*.*`, series(clearFonts, fonts));
+		series(clearImg, imagesConverter),
+	)
+	watch(`${path.svg.src}/**/*.svg`, svgConverter)
+	watch(`${path.fonts.src}/**/*.*`, series(clearFonts, fonts))
 	watch(
 		`${path.js.src}/**/*.js`,
-		series(bundleJsGenerator, htmlGenerator, styleGenerator)
-	);
+		series(bundleJsGenerator, htmlGenerator, styleGenerator),
+	)
 }
 
 exports.build = series(
@@ -163,8 +165,8 @@ exports.build = series(
 	svgConverter,
 	imagesConverter,
 	fonts,
-	favicon
-);
+	favicon,
+)
 
 exports.default = series(
 	clear,
@@ -175,5 +177,5 @@ exports.default = series(
 	imagesConverter,
 	fonts,
 	favicon,
-	parallel(server, watcher)
-);
+	parallel(server, watcher),
+)
