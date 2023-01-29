32419368db5dfdcc5a70fb2640239ce72cde9d29
Kirill Kolchanov <kirkol94@ya.ru>
2023-01-29 12:55:18 +0500

    кнопки на слайдере в продукте дня

--- src/js/bundle.js
+++ src/js/bundle.js
@@ -1,3 +1,4 @@
 //= 'header.js'
 //= 'intro.js'
 //= 'sale.js'
+//= 'product_of_day.js'
---
+++ src/js/product_of_day.js
@@ -0,0 +1,9 @@
+!(() => {
+    new Swiper('.js-product_of_day__slider', {
+        navigation: {
+            nextEl: '.js-product_of_day__next',
+            prevEl: '.js-product_of_day__prev',
+        },
+
+    })
+})()
